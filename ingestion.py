5c9b57d95b7e66e2477b805fbd13f2c4d6272afe
wkim10 <wonkim025@gmail.com>
2026-08-26 20:24:19 -0700

    finish ingestion implementation

--- ingestion.py
+++ ingestion.py
@@ -1,9 +1,29 @@
 import os
 from dotenv import load_dotenv
+from langchain_unstructured import UnstructuredLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
 
 
 load_dotenv()
 
 if __name__ == '__main__':
     print("Ingesting...")
+    loader = UnstructuredLoader(
+        file_path="/Users/won/Development/Udemy/LangChainCourse/langchain-course/mediumblog1.txt",
+        chunking_strategy="basic",
+        max_characters=1000000
+        )
+    document = loader.load()
 
+    print("splitting...")
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_documents(document)
+    print(f"created {len(texts)} chunks")
+
+    embeddings = OpenAIEmbeddings(openai_api_type=os.environ.get("OPENAI_API_KEY"))
+
+    print("ingesting...")
+    PineconeVectorStore.from_documents(texts, embeddings, index_name=os.environ['INDEX_NAME'])
+    print("finish")
